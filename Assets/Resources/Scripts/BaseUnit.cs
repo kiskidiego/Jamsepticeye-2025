f9368a7d523d4188a8884dd583c268f824d2f8fe
Diego Pérez <dperezrueda22@gmail.com>
2025-10-02 10:43:36 +0200

    Added attack speed calculations

--- Assets/Resources/Scripts/BaseUnit.cs
+++ Assets/Resources/Scripts/BaseUnit.cs
@@ -5,23 +5,26 @@ public abstract class BaseUnit : Hittable
 {
 
     [SerializeField] protected float _damage;
-    [SerializeField] protected float _attackSpeed;
+    [SerializeField] protected float _attackSpeed; // Attacks per second
     [SerializeField] protected float _movementSpeed;
     [SerializeField] protected float _range;
     [SerializeField] protected TargetingPriorities _targetingPriority;
     protected Hittable _target;
     protected float _rangeSquared;
+    protected float _attackCooldown; // Time between attacks
+    protected float _currentAttackCooldown; // Time left until next attack
 
     /// <summary>
-    /// Initializes the unit's current health, its squared size and its squared range. Can be overriden by derived classes.
+    /// Initializes the unit's current health, its squared size, its squared range and its attack cooldown. Can be overriden by derived classes.
     /// </summary>
     protected override void Start()
     {
         base.Start();
         _rangeSquared = _range * _range;
+        _attackCooldown = 1f / _attackSpeed;
     }
     /// <summary>
-    /// Default Unit behavior. Finds a target and attacks it if in range. Can be overriden by derived classes.
+    /// Default Unit behavior. Finds a target and tries to attack it if in range. Can be overriden by derived classes.
     /// </summary>
     protected virtual void Update()
     {
@@ -36,7 +39,7 @@ public abstract class BaseUnit : Hittable
         }
         else
         {
-            Attack();
+            CheckAttack();
         }
     }
 
@@ -48,6 +51,22 @@ public abstract class BaseUnit : Hittable
         if (_target != null)
         {
             transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    /// <summary>
+    /// Checks if the unit can attack based on its attack cooldown, and attacks if possible.
+    /// </summary>
+    protected void CheckAttack()
+    {
+        if (_currentAttackCooldown <= 0f)
+        {
+            Attack();
+            _currentAttackCooldown = _attackCooldown;
+        }
+        else
+        {
+            _currentAttackCooldown -= Time.deltaTime;
         }
     }
 

90b1fb12d59429e93b0ea95f523db28a4cb135ac
VicMelia <al415395@uji.es>
2025-10-03 20:52:01 +0200

    shield adjustments

--- Assets/Resources/VFX/Shield.cs
+++ Assets/Resources/VFX/Shield.cs
@@ -5,19 +5,29 @@ using System.Collections.Generic;
 public class Shield : MonoBehaviour
 {
     [SerializeField] float dissolveSpeed = 0.6f;
+    [SerializeField] float waveAmplitude = 1f;
+    [SerializeField] float pulseSpeed = 3f;
+    private Vector3 _baseScale;
     private Renderer _renderer;
 
     void Awake()
     {
         _renderer = GetComponent<Renderer>();
-        StartCoroutine(ActivateShield());
+        StartCoroutine(DeactivateShield());
+        _baseScale = transform.localScale;
     }
 
-    public IEnumerator ActivateShield()
+    private void Update()
+    {
+        float scale = 1 + Mathf.Sin(Time.time * pulseSpeed) * waveAmplitude;
+        transform.localScale = _baseScale * scale;
+    }
+
+    public IEnumerator DeactivateShield()
     {
         _renderer.material.SetFloat("_DissolveFactor", 0f);
         float start = 0f;
-        float target = 1f;
+        float target = 1.1f;
         float lerp = 0f;
         while(lerp < 1)
         {
@@ -26,9 +36,11 @@ public class Shield : MonoBehaviour
             yield return null;
         }
         _renderer.material.SetFloat("_DissolveFactor", target);
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(ActivateShield());
     }
 
-    public IEnumerator DissolveShield()
+    public IEnumerator ActivateShield()
     {
         _renderer.material.SetFloat("_DissolveFactor", 1f);
         float start = 1f;
